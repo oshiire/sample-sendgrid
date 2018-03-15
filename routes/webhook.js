6724c9e6827c8530c8177aa70f0d7cc67123b679
sho kisaragi <sho@oshiire.to>
2018-03-15 11:09:37 +0900

    using multer for multipart/form-data

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,10 +1,12 @@
+let multer = require('multer');
+let upload = multer();
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-  res.send('OK');
-  console.log(req);
+router.post('/', upload.fields([]), function(req, res, next) {
+  console.log(req.body);
+  res.sendStatus(200);
 });
 
 router.get('/', function(req, res, next) {
