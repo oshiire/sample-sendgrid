17d765ce5cd17e181648be70e0daf17c6241e0a4
sho kisaragi <sho@oshiire.to>
2018-03-15 10:01:50 +0900

    added POST for webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   res.send('OK');
+  console.log(req);
+});
+
+router.get('/', function(req, res, next) {
+  res.send('request OK');
   console.log(req);
 });
 
